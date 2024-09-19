e8375348471181a05a4fe5603052ab1fc51412b1
benjfrench <benfrerch@live.com.au>
2024-09-19 19:09:04 +0800

    models integration

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    playerController: require('./playerController')
+  }
--- models/index.js
+++ models/index.js
@@ -1,14 +1,17 @@
-"use strict"
-const Player = require("./player"); //require the model
+'use strict'
+const Coach = require('./coach');
+const Player = require('./player'); //require the model
+const Progress = require('./progress');
 
 async function init() {
-  await Player.sync(); // sync the model
-  // also sync any extra models here
-}
+   await Player.sync(); // sync the model
+   await Coach.sync();
+   await Progress.sync();
+};
 
 init();
 
 module.exports = {
-  Player, // export the model
-  // also export any extra models here
+   Player, Coach, Progress // export the model
+   // also export any extra models here
 };
